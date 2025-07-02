e48bc78b30a080715e483615be3192c90ed21e5c
RiyaLunagariya <149014724+RiyaLunagariya@users.noreply.github.com>
2025-07-02 17:24:18 +0530

    Remove Shiprocket routes and clean up index.js; update CSS for orders dashboard layout

--- Backend/routes/shiprocketRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { testShiprocketCredentials } = require('../controller/orderController');
-
-router.get('/test-credentials', testShiprocketCredentials);
-
-module.exports = router; 
