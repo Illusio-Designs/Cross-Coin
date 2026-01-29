0f4939949e810ac52a1d936ec1d20eb692673955
RiyaLunagariya <149014724+RiyaLunagariya@users.noreply.github.com>
2026-01-29 18:45:12 +0530

    microsoft clarity added

--- Frontend/src/pages/_document.jsx
+++ Frontend/src/pages/_document.jsx
@@ -24,6 +24,20 @@ export default function Document() {
         {/* Prevent dark mode */}
         <meta name="color-scheme" content="light" />
         <meta name="supported-color-schemes" content="light" />
+
+        {/* Microsoft Clarity */}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "seoi51zytnthis");
+            `,
+          }}
+        />
       </Head>
       <body>
         <noscript>
