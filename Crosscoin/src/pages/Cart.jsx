6269a42a424c51f27c1badfff9934db108e2ef3e
RiyaLunagariya <149014724+RiyaLunagariya@users.noreply.github.com>
2026-02-05 19:26:14 +0530

    update

--- Crosscoin/src/pages/Cart.jsx
+++ Crosscoin/src/pages/Cart.jsx
@@ -3,7 +3,6 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 import CartStep from '../components/checkout/CartStep';
 import QuantityOfferBar from '../components/cart/QuantityOfferBar';
-import PaymentModeOfferBar from '../components/cart/PaymentModeOfferBar';
 import { useCart } from '../context/CartContext';
 import { useRouter } from 'next/router';
 import { showSuccessToast } from '../utils/toast';
