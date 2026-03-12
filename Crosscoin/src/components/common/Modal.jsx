996633f0f22ca7163100308e8a4312020c20e05b
RushikeshZinzuvadiya <96772491+RushikeshZinzuvadiya@users.noreply.github.com>
2026-03-13 01:27:56 +0530

    update on button

--- Crosscoin/src/components/common/Modal.jsx
+++ Crosscoin/src/components/common/Modal.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Button from './Button';
+import { Button } from '../ui';
 import { IoClose } from 'react-icons/io5';
 import '../../styles/common/Modal.css';
 
