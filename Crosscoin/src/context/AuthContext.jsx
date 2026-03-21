d48eb8656c9af29abd176f20f7943a6e76196d87
RushikeshZinzuvadiya <96772491+RushikeshZinzuvadiya@users.noreply.github.com>
2026-03-21 19:32:15 +0530

    update in blog page

--- Crosscoin/src/context/AuthContext.jsx
+++ Crosscoin/src/context/AuthContext.jsx
@@ -117,7 +117,7 @@ function AuthProvider({ children }) {
 
     return (
         <AuthContext.Provider value={value}>
-            {!loading && children}
+            {children}
         </AuthContext.Provider>
     );
 }
