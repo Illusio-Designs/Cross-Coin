14f8b1915c0247efb996dba565189780f877a897
RushikeshZinzuvadiya <96772491+RushikeshZinzuvadiya@users.noreply.github.com>
2026-03-31 14:34:30 +0530

    feat: WhatsApp chat widget, WA Studio UI redesign, fix HMR cache issues

--- Crosscoin/src/pages/_app.jsx
+++ Crosscoin/src/pages/_app.jsx
@@ -96,6 +96,7 @@ import "../styles/dashboard/pages.css";
 import "../styles/dashboard/brands.css";
 import "../styles/dashboard/brandSettings.css";
 import "../styles/dashboard/brandTags.css";
+import "../styles/components/WhatsAppChat.css";
 import "../styles/dashboard/social.css";
 import "../styles/dashboard/whatsapp.css";
 // Additional page CSS
