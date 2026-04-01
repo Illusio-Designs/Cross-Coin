c05b2d05c9fd6e8465af96faa0fc8e94579bfcd5
RushikeshZinzuvadiya <96772491+RushikeshZinzuvadiya@users.noreply.github.com>
2026-04-01 16:35:55 +0530

    fix: use dangerouslySetInnerHTML in Script tags to fix syntax error in bundled output

--- Crosscoin/src/components/common/Analytics.jsx
+++ Crosscoin/src/components/common/Analytics.jsx
@@ -43,8 +43,8 @@ const Analytics = () => {
   return (
     <>
       {/* ── Facebook Pixel ── */}
-      <Script id="fb-pixel" strategy="afterInteractive">
-        {`
+      <Script id="fb-pixel" strategy="afterInteractive"
+        dangerouslySetInnerHTML={{ __html: `
           !function(f,b,e,v,n,t,s)
           {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
           n.callMethod.apply(n,arguments):n.queue.push(arguments)};
@@ -55,43 +55,37 @@ const Analytics = () => {
           'https://connect.facebook.net/en_US/fbevents.js');
           fbq('init', '${FB_PIXEL_ID}');
           fbq('track', 'PageView');
-        `}
-      </Script>
+        `}}
+      />
       <noscript>
         {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img
-          height="1"
-          width="1"
-          style={{ display: "none" }}
+        <img height="1" width="1" style={{ display: "none" }}
           src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
           alt=""
         />
       </noscript>
 
       {/* ── Google Analytics 4 ── */}
-      <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
+      <Script src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`} strategy="afterInteractive" />
+      <Script id="google-analytics" strategy="afterInteractive"
+        dangerouslySetInnerHTML={{ __html: `
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
           gtag('config', '${GA_ID}', { send_page_view: true });
-        `}
-      </Script>
+        `}}
+      />
 
       {/* ── Microsoft Clarity ── */}
-      <Script id="microsoft-clarity" strategy="afterInteractive">
-        {`
+      <Script id="microsoft-clarity" strategy="afterInteractive"
+        dangerouslySetInnerHTML={{ __html: `
           (function(c,l,a,r,i,t,y){
             c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
             t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
             y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
           })(window, document, "clarity", "script", "${CLARITY_ID}");
-        `}
-      </Script>
+        `}}
+      />
     </>
   );
 };
